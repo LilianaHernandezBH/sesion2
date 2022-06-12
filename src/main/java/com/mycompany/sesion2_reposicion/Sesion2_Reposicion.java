f82d7f4491e0c362d3cca2c320a1c68194a0899c
LilianaHernandez <106934813+hansun8390609@users.noreply.github.com>
2022-06-12 17:29:42 -0500

    Inicio
    Inicio

---
+++ src/main/java/com/mycompany/sesion2_reposicion/Sesion2_Reposicion.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.sesion2_reposicion;
+
+/**
+ *
+ * @author Liliana
+ */
+public class Sesion2_Reposicion {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
